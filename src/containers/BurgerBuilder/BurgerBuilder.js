6916e93d852c5abc143a0ce17da4a48cdc283e8a
Oleksii Kuznietsov <okuznetsov@flexservice.com>
2020-01-10 14:16:02 +0200

    Add last

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -34,6 +34,7 @@ class BurgerBuilder extends Component {
             })
             .catch( error => {
                 this.setState( { error: true } );
+				console.log(error);
             } );
     }
 
@@ -108,9 +109,11 @@ class BurgerBuilder extends Component {
 		axios.post( '/orders.json', order )
 			.then( response => {
 				this.setState( { loading: false, purchasing: false } );
+				console.log(response);
 			} )
 			.catch( error => {
 				this.setState( { loading: false, purchasing: false } );
+				console.log(error);
 			} );
     };
 
