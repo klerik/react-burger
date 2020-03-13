1b9d5a7323e674af8cc34ee76f621d60f29b1cda
kleri <NTjwRq6DNTjwRq6D>
2020-03-13 08:18:05 +0200

    Beginr adv redux

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import * as serviceWorker from './serviceWorker';
 
 import './index.css';
 import App from './App';
-import reducer from './store/reducer';
+import burgerBuilder from './store/reducers/burgerBuilder';
 
-const store = createStore(reducer);
+const store = createStore(
+    burgerBuilder, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 const app = (
     <Provider store={store}>
